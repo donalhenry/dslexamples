b0b91b1ef6a24f29f04e4257869cd952913dc64e
Don Henry <donal.henry@turner.com>
2016-04-27 20:55:31 -0400

    Added new example

---
+++ example4/cookingdsl.groovy
@@ -0,0 +1,78 @@
+apply plugin: CookingDslPlugin
+
+class CookingDslPlugin implements Plugin<Project> {
+  void apply(Project project) {
+    project.extensions.create('devconfig', CookingExtension)
+    project.devconfig.extensions.repositories = new Repositories()
+    def teams = project.container(Team) { name->
+      new Team(name, project)
+    }
+    project.devconfig.extensions.teams = teams
+  }
+}
+
+class CookingExtension {
+  CookingExtension() {
+  }
+}
+
+class Repositories {
+  YumRepo yum
+
+  Repositories() {
+    this.yum = new YumRepo()
+  }
+
+  void yum(Closure closure) {
+    yum.configure(closure)
+  }
+}
+
+class YumRepo {
+  String host
+
+  YumRepo() {
+  }
+
+  void configure(Closure closure) {
+    ConfigureUtil.configure(closure, this)
+  }
+
+  String getBaseUrl() {
+    "http://$host"
+  }
+}
+
+class Team {
+  String name
+  Project project
+  Yum yum
+
+  Team(String name, Project project) {
+    this.name = name
+    this.project = project
+    this.yum = new Yum(name, project)
+  }
+
+  void yum(Closure closure) {
+    yum.configure(closure)
+  }
+}
+
+class Yum {
+  String path
+  Project project
+
+  Yum(String teamName, Project project) {
+    this.path = "/opt/teamrepos/$teamName/rhel/6"
+    this.project = project
+  }
+
+  void configure(Closure closure) {
+    ConfigureUtil.configure(closure, this)
+  }
+
+  String getUrl() {
+    "$project.devconfig.repositories.yum.baseUrl/$path"
+  }
+}
