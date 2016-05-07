fc29098eacdd857a76a61a3d01700147fec40c70
Don Henry <donal.henry@turner.com>
2016-05-07 17:21:51 -0400

    Checking in first attempt; probably not what will work

--- example6/buildSrc/src/main/groovy/com/donalhenry/gradle/example6/CookingDslPlugin.groovy
+++ example6/buildSrc/src/main/groovy/com/donalhenry/gradle/example6/CookingDslPlugin.groovy
@@ -31,7 +31,46 @@ class CookingDslPlugin implements Plugin<Project> {
 
 class CookingExtension {
   String defaultDomain
+  Environment dev, qa
 
   CookingExtension() {
+    dev = new Environment()
+    qa = new Environment()
+  }
+
+  void dev(Closure closure) {
+    dev.configure(closure)
+  }
+
+  void qa(Closure closure) {
+    qa.configure(closure)
+  }
+}
+
+class Environment {
+  List<ChefNode> chefNodes = []
+
+  Environment() {
+  }
+
+  void configure(Closure closure) {
+    ConfigureUtil.configure(closure, this)
+  }
+
+  void chefNode(Closure closure) {
+    def chefNode = new ChefNode()
+    chefNode.configure(closure)
+    chefNodes.add(chefNode)
+  }
+}
+
+class ChefNode {
+  String host
+
+  ChefNode () {
+  }
+
+  void configure(Closure closure) {
+    ConfigureUtil.configure(closure, this)
   }
 }
